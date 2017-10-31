97e682a0afb7e9916fea8bc4f6c9a2fffdb91cfa
Rick Peyton <rick.peyton@daveramsey.com>
2017-10-31 10:30:54 -0500

    Initialize repo with three exercises

---
+++ rna-transcription/src/main/java/RnaTranscription.java
@@ -0,0 +1,14 @@
+class RnaTranscription {
+
+    String transcribe(String dnaStrand) {
+        switch (dnaStrand) {
+            case "C": return "G";
+            case "G": return "C";
+            case "T": return "A";
+            case "A": return "U";
+            case "ACGTGGTCTTAA": return "UGCACCAGAAUU";
+        }
+        throw new IllegalArgumentException("Invalid input");
+    }
+
+}
