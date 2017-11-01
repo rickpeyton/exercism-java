9940d225d452d9afa7221eccb36835977fafe38b
Rick Peyton <rick.peyton@daveramsey.com>
2017-11-01 15:36:04 -0500

    Inspired by lambda example

--- pangram/src/main/java/PangramChecker.java
+++ pangram/src/main/java/PangramChecker.java
@@ -1,17 +1,11 @@
 public class PangramChecker {
 
     public boolean isPangram(String input) {
-        String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
-        char[] alphabetArray = ALPHABET.toCharArray();
-        for (char ch : alphabetArray) {
-            if (notInString(ch, input)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public boolean notInString(char ch, String input) {
-        return input.toLowerCase().indexOf(ch) < 0;
+        input = input.toLowerCase();
+        return input
+                .chars()
+                .filter(i -> i >= 'a' && i <= 'z')
+                .distinct()
+                .count() == 26;
     }
 }
