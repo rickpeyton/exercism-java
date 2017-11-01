3eb601cecbf07a27dcee48aaece37180ab85aaa2
Rick Peyton <rick.peyton@daveramsey.com>
2017-11-01 13:35:36 -0500

    Refactor RnaTranscription to use StringBuilder

--- rna-transcription/src/main/java/RnaTranscription.java
+++ rna-transcription/src/main/java/RnaTranscription.java
@@ -7,8 +7,7 @@ class RnaTranscription {
             case "T": return "A";
             case "A": return "U";
             case "ACGTGGTCTTAA": return "UGCACCAGAAUU";
+            default: throw new IllegalArgumentException("Invalid input");
         }
-        throw new IllegalArgumentException("Invalid input");
     }
-
 }
