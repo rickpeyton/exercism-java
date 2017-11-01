ae57ecba341fcc982282c5eba4cc073c3890f880
Rick Peyton <rick.peyton@daveramsey.com>
2017-11-01 14:12:43 -0500

    Looping through the dnaStrand instead of switching on every possibility

--- rna-transcription/src/main/java/RnaTranscription.java
+++ rna-transcription/src/main/java/RnaTranscription.java
@@ -1,13 +1,25 @@
 class RnaTranscription {
-
     String transcribe(String dnaStrand) {
-        switch (dnaStrand) {
-            case "C": return "G";
-            case "G": return "C";
-            case "T": return "A";
-            case "A": return "U";
-            case "ACGTGGTCTTAA": return "UGCACCAGAAUU";
-            default: throw new IllegalArgumentException("Invalid input");
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < dnaStrand.length(); i++) {
+           char ch = dnaStrand.charAt(i);
+           sb.append(transcribe(ch));
+        }
+        return sb.toString();
+    }
+
+    char transcribe(char subString) {
+        switch (subString) {
+            case 'C':
+                return 'G';
+            case 'G':
+                return 'C';
+            case 'T':
+                return 'A';
+            case 'A':
+                return 'U';
+            default:
+                throw new IllegalArgumentException("Invalid input");
         }
     }
 }
