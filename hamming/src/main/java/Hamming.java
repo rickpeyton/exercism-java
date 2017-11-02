6a128b087e26d7fd39e819677892b747feae0404
Rick Peyton <rick.peyton@daveramsey.com>
2017-11-02 08:17:16 -0500

    Hamming coding quiz

---
+++ hamming/src/main/java/Hamming.java
@@ -0,0 +1,24 @@
+public class Hamming {
+    private char[] leftStrandArr;
+    private char[] rightStrandArr;
+
+    Hamming(String leftStrand, String rightStrand) {
+        if (leftStrand.length() != rightStrand.length()) {
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }
+        leftStrandArr = leftStrand.toCharArray();
+        rightStrandArr = rightStrand.toCharArray();
+        getHammingDistance();
+    }
+
+    int getHammingDistance() {
+        int distance = 0;
+        for (int i = 0; i < leftStrandArr.length; i++) {
+            if (leftStrandArr[i] != rightStrandArr[i]) {
+                distance += 1;
+            }
+        }
+        return distance;
+    }
+
+}
